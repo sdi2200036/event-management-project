ab01bf317fa22dddedf44cb6ddafc44663645a58
Giorgos Delgas <delgasgiorgos@gmail.com>
2026-07-12 22:23:19 +0300

    updated backend api

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	apiUrl: 'https://event-management-project-production.up.railway.app'
+	apiUrl: 'https://event-management-project-production.up.railway.app/api'
 };
